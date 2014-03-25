b7af0678e00fd1603cda245de0d8a6dc12cc487c
John Ryan <jcryan85@gmail.com>
2014-03-25 12:19:50 -0700

    updated services.js

--- www/js/services.js
+++ www/js/services.js
@@ -9,11 +9,10 @@ angular.module('starter.services', [])
     { id: 1, title: 'BroHacks', description: 'Brohacking all over the world!' },
     { id: 2, title: 'RocketU', description: 'Bootcamp for bros who wanna step up there game' },
     { id: 3, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' },
-    { id: 4, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' },
-    { id: 5, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' },
-    { id: 6, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' },
-    { id: 7, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' },
-    { id: 8, title: 'Uber', description: 'Distrupting the taxi industry to help people who travel a lot.' }
+    { id: 4, title: 'Rocket Space', description: 'Accelerator for high-growth startups that have closed series A funding' },
+    { id: 5, title: 'Palantir', description: 'Distrupting the taxi industry to help people who travel a lot.' },
+    { id: 6, title: 'YetiHQ', description: 'Distrupting the taxi industry to help people who travel a lot.' },
+    { id: 7, title: 'ParkME', description: 'Distrupting the taxi industry to help people who travel a lot.' }
   ];
 
   return {
